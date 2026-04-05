a3183f88e87ac1ee1c9dcb7dfaa25a2724eb9908
Mikael Lindqvist <li.mikael@gmail.com>
2026-04-05 18:46:08 +0800

    testing...

--- src/peabind/peabind-wasm.js
+++ src/peabind/peabind-wasm.js
@@ -4,94 +4,106 @@ import {DeclaredError} from "../utils/js-util.js";
 import path from "path";
 import fs from "fs";
 import os from "os";
+import {autoIndent} from "../utils/auto-indent.js";
 
-function peabindWasmWrapper({idl, projectName, prefix}) {
-    let js=`
-        const wasmUrl = new URL('./${projectName}.wasm', import.meta.url);
-        let wasmBytes;
-        if (typeof window === 'undefined') {
-            let fs=await import('fs');
-            wasmBytes=await fs.promises.readFile(wasmUrl);
-        } else {
-            let res=await fetch(wasmUrl);
-            wasmBytes=await res.arrayBuffer();
-        }
+class PeabindWasmBuilder {
+    constructor({idl, prefix, projectName}) {
+        this.idl=idl;
+        this.prefix=prefix;
+        this.projectName=projectName;
 
-        const imports = {
-            wasi_snapshot_preview1: {
-                proc_exit: () => {},        // called on program exit
-                fd_write: () => 0,          // called for stdout/stderr writes
-                fd_close: () => 0,          // optional if you don’t use files
-                fd_seek: () => 0,           // optional
-                fd_fdstat_get: () => 0,     // optional
-                environ_sizes_get: () => 0, // optional
-                environ_get: () => 0,       // optional
-            }
-        };
-
-        let {instance}=await WebAssembly.instantiate(wasmBytes, imports);
-        let exp=instance.exports;
-    `;
-
-    for (let func of idl.functions) {
-        js+=`
-            export const ${func.name}=exp.${prefix}${func.name};
-        `;
+        if (!this.prefix)
+            this.prefix=this.projectName.replaceAll(".","_")+"_";
     }
 
-    for (let cls of idl.classes) {
-        js+=`
-            export class ${cls.name} {
-                constructor() {
-                    this._handle=exp.${cls.name}_new();
+    getExportedFunctionNames() {
+        let exportedFunctionNames=[];
+
+        for (let func of this.idl.functions)
+            exportedFunctionNames.push(`_${this.prefix}${func.name}`);
+
+        for (let cls of this.idl.classes) {
+            exportedFunctionNames.push(`_${cls.name}_new`);
+            for (let method of cls.methods)
+                exportedFunctionNames.push(`_${cls.name}_${method.name}`);
+        }
+
+        return exportedFunctionNames;
+    }
+
+    createWasmStub() {
+
+        return autoIndent(`
+            ${this.idl.include.map(i=>`#include "${i}"`).join("\n")}
+
+            extern "C" {
+                ${this.idl.functions.map(func=>`
+                    ${func.return.type} ${this.prefix}${func.name}(
+                        ${func.args.map((arg,i)=>`${arg.type} arg_${i}`).join(",")}) {
+                    return ${func.name}(${func.args.map((arg,i)=>`arg_${i}`).join(",")});
+                }`).join("\n")}
+
+                ${this.idl.classes.map(cls=>`
+                    ${cls.name}* ${cls.name}_new() {
+                        return new ${cls.name}();
+                    }
+
+                    ${cls.methods.map(method=>`
+                        int ${cls.name}_${method.name}(${cls.name}* instance) {
+                            return instance->${method.name}();
+                        }
+                    `).join("\n")}
+                `).join("\n")}
+            }
+        `);
+    }
+
+    createWasmWrapper() {
+        return autoIndent(`
+            const wasmUrl = new URL('./${this.projectName}.wasm', import.meta.url);
+            let wasmBytes;
+            if (typeof window === 'undefined') {
+                let fs=await import('fs');
+                wasmBytes=await fs.promises.readFile(wasmUrl);
+            } else {
+                let res=await fetch(wasmUrl);
+                wasmBytes=await res.arrayBuffer();
+            }
+
+            const imports = {
+                wasi_snapshot_preview1: {
+                    proc_exit: () => {},        // called on program exit
+                    fd_write: () => 0,          // called for stdout/stderr writes
+                    fd_close: () => 0,          // optional if you don’t use files
+                    fd_seek: () => 0,           // optional
+                    fd_fdstat_get: () => 0,     // optional
+                    environ_sizes_get: () => 0, // optional
+                    environ_get: () => 0,       // optional
                 }
-        `;
+            };
 
-        for (let method of cls.methods) {
-            js+=`${method.name}() {
-                return exp.${cls.name}_${method.name}(this._handle);
-            }\n`;
-        }
+            let {instance}=await WebAssembly.instantiate(wasmBytes, imports);
+            let exp=instance.exports;
 
-        js+=`};\n`;
+            ${this.idl.functions.map(func=>`
+                export const ${func.name}=exp.${this.prefix}${func.name};
+            `).join("\n")}
+
+            ${this.idl.classes.map(cls=>`
+                export class ${cls.name} {
+                    constructor() {
+                        this._handle=exp.${cls.name}_new();
+                    }
+
+                    ${cls.methods.map(method=>`
+                        ${method.name}() {
+                            return exp.${cls.name}_${method.name}(this._handle);
+                        }
+                    `).join("\n")}
+                }
+            `).join("\n")}
+        `);
     }
-
-    return js;
-}
-
-export async function peabindWasmStub({idl, projectName, prefix, exportedFunctionNames}) {
-    let src=``;
-    src+=idl.include.map(i=>`#include "${i}"\n`);
-
-    src+=`extern "C" {\n`;
-
-    for (let func of idl.functions) {
-        exportedFunctionNames.push(`_${prefix}${func.name}`);
-        src+=`
-            ${func.return.type} ${prefix}${func.name}(
-                    ${func.args.map((arg,i)=>`${arg.type} arg_${i}`).join(",")}) {
-                return ${func.name}(${func.args.map((arg,i)=>`arg_${i}`).join(",")});
-            }
-        `;
-    }
-
-    for (let cls of idl.classes) {
-        exportedFunctionNames.push(`_${cls.name}_new`);
-        src+=`${cls.name}* ${cls.name}_new() {
-            return new ${cls.name}();
-        }\n`;
-
-        for (let method of cls.methods) {
-            exportedFunctionNames.push(`_${cls.name}_${method.name}`);
-            src+=`int ${cls.name}_${method.name}(${cls.name}* instance) {
-                return instance->${method.name}();
-            }\n`;
-        }
-    }
-
-    src+=`}\n`;
-
-    return src;
 }
 
 export async function peabindWasm({idl, sources, output, prefix}) {
@@ -104,20 +116,10 @@ export async function peabindWasm({idl, sources, output, prefix}) {
 
     let outputBase=path.join(outputPath.dir,outputPath.name);
     let projectName=outputPath.name;
+    let builder=new PeabindWasmBuilder({idl, projectName});
 
-    if (!prefix)
-        prefix=projectName.replaceAll(".","_")+"_";
-
-    idl.prefix=prefix;
-
-    let exportedFunctionNames=[]; //idl.functions.map(f=>"_"+f.name);
-
-    let stub=await peabindWasmStub({idl, projectName, exportedFunctionNames, prefix});
     let stubFn=path.join(os.tmpdir(), "peabind-stub.cpp");
-    fs.writeFileSync(stubFn,stub);
-
-    //console.log(stub);
-    //console.log(exportedFunctionNames);
+    fs.writeFileSync(stubFn,builder.createWasmStub());
 
     await runCommand("emcc",[
         ...sources,
@@ -125,12 +127,10 @@ export async function peabindWasm({idl, sources, output, prefix}) {
         "-I",idlDir,
         "-o",outputBase+".wasm",
         "-s","STANDALONE_WASM=1",
-        "-s",`EXPORTED_FUNCTIONS=${exportedFunctionNames.join(",")}`,
+        "-s",`EXPORTED_FUNCTIONS=${builder.getExportedFunctionNames().join(",")}`,
         "--no-entry"
     ]);
 
-    let js=peabindWasmWrapper({idl, projectName, prefix});
-    fs.writeFileSync(path.join(outputPath.dir,outputPath.name+".js"),js);
-
-    //console.log(js);
+    let wrapperFn=path.join(outputPath.dir,outputPath.name+".js");
+    fs.writeFileSync(wrapperFn,builder.createWasmWrapper());
 }
