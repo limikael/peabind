b643b8da542684ba30ad7765b4ab6f1ad70b4682
Mikael Lindqvist <li.mikael@gmail.com>
2026-04-28 13:19:23 +0800

    wip

--- js/peabind/ext.js
+++ js/peabind/ext.js
@@ -26,3 +26,15 @@ if (!fs.existsSync(path.join(ext,"quickjs-2025-09-13"))) {
 }
 
 await runCommand("make",[],{cwd: path.join(ext,"quickjs-2025-09-13")});
+
+let downloadQFile=path.join(ext,"mquickjs.zip");
+if (!fs.existsSync(downloadQFile)) {
+	await runCommand("curl",[
+		"https://github.com/bellard/mquickjs/archive/refs/heads/main.zip",
+		"-L","-o",downloadQFile
+	]);
+}
+
+if (!fs.existsSync(path.join("ext","mquickjs-main"))) {
+	await runCommand("unzip",["mquickjs.zip"],{cwd: ext});
+}
