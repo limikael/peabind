b4b01550fbe371296b75f28bd21577ea9af51de1
Mikael Lindqvist <li.mikael@gmail.com>
2026-05-10 20:31:50 +0800

    fixes

--- test/test-mqjs-basic.cpp
+++ test/test-mqjs-basic.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include "basic.out.h"
 
-#include "jsval.h"
 #define MEMSIZE 65536
 
 void test_mqjs_basic() {
@@ -13,11 +12,25 @@ void test_mqjs_basic() {
     void *mem=malloc(MEMSIZE);
     JSContext *ctx=JS_NewContext(mem, MEMSIZE, basic_get_stdlib());
 
-    basic_init_jsval();
-    //jsvalEval("hello(1,2)");
+    basic_init(ctx);
+    JSVAL v=jsvalEval("hello(5,2)");
+    char s[256];
+    jsvalReadString(v,s);
+    assert(std::string(s)=="7");
     basic_exit();
 
-    jsvalMqjsExit();
     JS_FreeContext(ctx);
     free(mem);
+}
+
+void test_mqjs_basic_jsval() {
+    printf("- basic mqjs bindings with borrowed jsval\n");
+    jsvalMqjsInit(MEMSIZE,basic_get_stdlib());
+    basic_init_jsval();
+    JSVAL v=jsvalEval("hello(5,2)");
+    char s[256];
+    jsvalReadString(v,s);
+    assert(std::string(s)=="7");
+    basic_exit();
+    jsvalMqjsExit();
 }
--- test/testmain-mquickjs.cpp
+++ test/testmain-mquickjs.cpp
@@ -4,6 +4,7 @@ void test_jsval_mqjs_basic();
 void test_jsval_size();
 void test_jsval_mqjs_borrow();
 void test_mqjs_basic();
+void test_mqjs_basic_jsval();
 
 int main() {
 	printf("Running mquickjs tests...\n");
@@ -11,7 +12,8 @@ int main() {
 	test_jsval_mqjs_basic();
 	test_jsval_size();
 	test_jsval_mqjs_borrow();
-	//test_mqjs_basic();
+	test_mqjs_basic();
+	test_mqjs_basic_jsval();
 
 	return 0;
 }
