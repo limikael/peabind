349bb1dcf5bfd3ab5d43063f04778b742d4f74f0
Mikael Lindqvist <li.mikael@gmail.com>
2026-06-26 09:40:46 +0800

    async cleanup

--- include/async_primitives.hpp
+++ include/async_primitives.hpp
@@ -14,7 +14,6 @@ class Dispatcher {
         int handle;
         std::function<void(Args...)> fn;
         std::function<void()> destructor;
-        //uint64_t idInt;
     };
 
     std::vector<Listener> listeners;
@@ -33,7 +32,7 @@ public:
 
     int on(std::function<void(Args...)> listener) {
         int handle = nextHandle++;
-        listeners.push_back({handle, std::move(listener), nullptr/*, 0*/});
+        listeners.push_back({handle, std::move(listener), nullptr});
         return handle;
     }
 
@@ -44,24 +43,6 @@ public:
             }
         }
     }
-
-    /*int getHandleByIdInt(uint64_t idInt) {
-        for (auto it = listeners.begin(); it != listeners.end(); ++it) {
-            if (it->idInt == idInt) {
-                return it->handle;
-            }
-        }
-
-        return 0;
-    }
-
-    void setIdInt(int handle, uint64_t idInt) {
-        for (auto it = listeners.begin(); it != listeners.end(); ++it) {
-            if (it->handle == handle) {
-                it->idInt=idInt;
-            }
-        }
-    }*/
 
     void off(int handle) {
         for (auto it = listeners.begin(); it != listeners.end(); ++it) {
